6cc51b8aec01d3ad830b64028aa4a58fb3ee5c77
Jarek <39769929+JarekWysocki@users.noreply.github.com>
2021-03-23 14:55:37 +0100

    Add files via upload

--- connect.php
+++ connect.php
@@ -5,4 +5,13 @@ $db_user = "jaroslawwysocki";
 $db_password = "Agnat2019!";
 $db_name = "jarekbaza123";
 
+try
+    {
+        $pdo = new PDO('mysql:host='. $host .';dbname='.$db_name, $db_user, $db_password);
+    }
+    catch (PDOException $e)
+    {
+        exit('Error Connecting To DataBase');
+    }
+
 ?>
