01382175ad2149cf75e0be317014ee87dd6d33a4
Jarek <39769929+JarekWysocki@users.noreply.github.com>
2021-03-31 13:00:30 +0200

    Add files via upload

--- data_delete.php
+++ data_delete.php
@@ -2,10 +2,18 @@
 class DataDelete {
     public function __construct($pdo, $thisId) {
         $this->pdo = $pdo;
-        $this->thisId = $thisId;
-      }
+        $this->thisId = $thisId;        
+    }
+function getNameImg() {
+    $result = $this->pdo->prepare('SELECT img FROM tab1 WHERE id = '.$this->thisId.''); 
+    $result->execute();
+    return json_encode ($result->fetchAll(PDO::FETCH_ASSOC)[0]{'img'});
+}
 function deleteData() {
-$this->pdo->query('DELETE FROM tab1 WHERE id = '.$this->thisId.'');
+$nameFile = $this->getNameImg();
+$nameFile = substr($nameFile, 1, -1);
+unlink('img/'.$nameFile);
+ $this->pdo->query('DELETE FROM tab1 WHERE id = '.$this->thisId.'');
 }
 function __destruct()
 {   
@@ -16,4 +24,5 @@ require_once 'connect.php';
 $thisId = $_POST['thisId'];
 $db = new DataDelete($pdo, $thisId);
 $db->deleteData();
+
 ?>
