8e155d86690f2ed779a4de22b9e284ec14f0c799
Jarek <39769929+JarekWysocki@users.noreply.github.com>
2021-04-02 09:20:08 +0200

    Add files via upload

--- ajax.js
+++ ajax.js
@@ -7,13 +7,13 @@ $(document).ready(function () {
     var change = e.target.id;
     switch (change) {
       case 'newFname':
-      var newFname = prompt("Edytuj imię");
+      var newFname = prompt("Edit first name");
       break;
       case 'newLname' :
-      var newLname = prompt("Edytuj nazwisko"); 
+      var newLname = prompt("Edit last name"); 
       break;
       case 'newPass' :
-      var newPass = prompt("Edytuj hasło"); 
+      var newPass = prompt("Edit password"); 
       break; 
     }
     
@@ -21,55 +21,56 @@ $(document).ready(function () {
     $.ajax
     ({
       type: "POST",
-      url: "edit_data.php", 
-      data: {"thisId": thisId, "newFname": newFname, "newLname": newLname, "newPass": newPass},
+      url: "controll.php", 
+      data: {"thisId": thisId, "newFname": newFname, "newLname": newLname, "newPass": newPass, "choice" : "edit"},
       success: function (data) {
      getRecord();
+     alert(data);
       }
     });
   }
   }
   deleteRecord = (e) => {
-    var thisId = e.target.id;
     $.ajax
     ({
       type: "POST",
-      url: "data_delete.php",
-      data: { "thisId": thisId },
+      url: "controll.php",
+      data: { "thisId": e.target.id, "choice" : "delete" },
       success: function (data) {
-        
-     alert("Usuniete");
-     getRecord();
+        getRecord();  
+     alert(data);
       }
     });
   }
   sendRecord = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     var formData = new FormData();
-    var fname = $('#fname').val();
-    var lname = $('#lname').val();
-    var pass = $('#pass').val();
-    var image = $("#image")[0].files[0];
-    formData.append("fname", fname);
-    formData.append("lname", lname);
-    formData.append("pass", pass);
-    formData.append("name", image);
+    formData.append("choice", e.target.id);
+    formData.append("fname", $('#fname').val());
+    formData.append("lname", $('#lname').val());
+    formData.append("pass", $('#pass').val());
+    formData.append("name", $("#image")[0].files[0]);
     $.ajax
       ({
         type: "POST",
-        url: "to_database.php",
+        url: "controll.php",
         data: formData,
         processData : false,
         contentType : false,
         success: function (data) {
           $('input[type="text"]').val('');
           getRecord();
-          alert("data send");
+          alert(data);
         }
       });
   }
   getRecord = (e) => {
-    $.get("from_database.php", function(data) {
+    $.ajax
+    ({
+      type: "POST",
+      url: "controll.php",
+      data: {"choice": "get"},
+      success: function (data) {   
       data = JSON.parse(data);
       if ($('.data').length) {
       $(".data").remove();
@@ -78,24 +79,32 @@ $(document).ready(function () {
         $('#demo').append('<table class="table"><tr><td>Id</td><td>Foto</td><td>Imie</td><td>Nazwisko</td><td>Hasło</td><td>Data utworzenia</td><td>Edycja</td></tr>');
       }
         data.forEach(function(atrb) {
-         
-       $(".table").append('<tr class="data"><td> '+ atrb.id +'</td><td><img src="https://'+ window.location.hostname + '/img/' + atrb.img +'"</td><td>'+ atrb.fname + 
+         atrb.img ?
+       $(".table").append('<tr class="data"><td> '+ atrb.id +'</td><td><img src="http://'+ window.location.hostname + '/img/' + atrb.img +'"></td><td>'+ atrb.fname + 
        '</td><td>'+ atrb.lname +'</td><td>'+ atrb.pass +'</td><td>'+ atrb.create + 
        '</td><td><span id="'+ atrb.id + 
-       '" class="delete">Usuń</span></br><span id="newFname" class="editData">Edytuj imię</span></br><span id="newLname" class="editData">Edytuj nazwisko</span></br><span id="newPass" class="editData">Edytuj hasło</span></td></tr></table>');
-       
+       '" class="delete">Usuń</span></br><span id="newFname" class="editData">Edytuj imię</span></br><span id="newLname" class="editData">Edytuj nazwisko</span></br><span id="newPass" class="editData">Edytuj hasło</span></td></tr></table>'):
+       $(".table").append('<tr class="data"><td> '+ atrb.id +'</td><td>Brak foto</td><td>'+ atrb.fname + 
+       '</td><td>'+ atrb.lname +'</td><td>'+ atrb.pass +'</td><td>'+ atrb.create + 
+       '</td><td><span id="'+ atrb.id + 
+       '" class="delete">Usuń</span></br><span id="newFname" class="editData">Edytuj imię</span></br><span id="newLname" class="editData">Edytuj nazwisko</span></br><span id="newPass" class="editData">Edytuj hasło</span></td></tr></table>') ; 
       })
       $('.delete').on("click", deleteRecord);
       $('.editData').on("click", editData);
-      
-    })
-  }
-  randomPass = () => {
-    $.get("randomPass.php", function(data) {
-    $('#pass').val(data);
-  }
-    
-    )}
+      }
+      })
+      }
+  randomPass = (e) => {
+    $.ajax
+    ({
+      type: "POST",
+      url: "controll.php", 
+      data: {"choice": e.target.id},
+      success: function (data) {
+        $('#pass').val(data);
+      }
+    });
+    }
 
     $('#send').on("click", sendRecord);
     $('#get').on("click", getRecord);
