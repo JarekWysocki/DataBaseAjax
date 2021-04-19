ffd841990de9638a6045414eadb56a77f86e768d
Jarek <39769929+JarekWysocki@users.noreply.github.com>
2021-04-19 10:44:48 +0200

    Add files via upload

--- islog.php
+++ islog.php
@@ -1,12 +1,10 @@
 <?php
 include 'connect.php';
 $myname = $_POST['myname'];
-if (($_POST['status'] == 0)) {
-    $activ = $pdo->prepare("UPDATE tab1 SET online=0 WHERE fname='$myname'"); 
-    $activ->execute();    
-}
-if (($_POST['status'] == 1)) {
-    $activ = $pdo->prepare("UPDATE tab1 SET online=1 WHERE fname='$myname'"); 
-    $activ->execute();    
-}
+$time = time();
+
+$activ = $pdo->prepare("UPDATE tab1 SET online=$time WHERE fname='$myname'"); 
+$activ->execute();    
+
+
 ?>
