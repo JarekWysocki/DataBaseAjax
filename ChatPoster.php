339f18980d0802ae8bbea0b447783f8129b76a8a
Jarek <39769929+JarekWysocki@users.noreply.github.com>
2021-05-04 11:29:13 +0200

    Add files via upload

--- ChatPoster.php
+++ ChatPoster.php
@@ -1,25 +1,48 @@
 <?php
    require_once 'connect.php';
-   
-    if(isset($_POST['text']) && isset($_POST['name'])) {
+    if(isset($_POST['text']) && isset($_POST['nameid'])) {
       $text = strip_tags(stripslashes($_POST['text']));
-      $name = strip_tags(stripslashes($_POST['name']));
-
-      if(!empty($text) && !empty($name)) {
-       
-
-
+      $nameid = strip_tags(stripslashes($_POST['nameid']));
+      $img = $_FILES['img'];
+      $expl = explode('.', $img['name']);
+      $extension_of_file = array_pop($expl); 
+      if ($img['size'] > 5000000) exit('Max size limit exceeded (5Mb)'); 
+      $allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif', '','image/heic', 'image/jpg'];
+            if (!in_array($img['type'], $allowedMimeTypes)) exit('Only JPEG, PNG and GIFs are allowed');    
+            if ($img['size'] > 1) {
+              $new_name = (bin2hex(openssl_random_pseudo_bytes(15, $cstrong))).".".$extension_of_file;
+              }
+              else {
+                $new_name = '';
+              }
+        @move_uploaded_file($img['tmp_name'], 'img/'.$new_name);
+            if(!empty($text) && !empty($nameid)) {
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $stmt = $pdo->prepare("INSERT INTO messages (name, message) VALUES (:name, :message)");
-        $stmt->bindParam(':name', $name);
+        $stmt = $pdo->prepare("INSERT INTO messages (name, message, image) VALUES (:name, :message, :image)");
+        $stmt->bindParam(':name', $nameid);
         $stmt->bindParam(':message', $text);
-        if($stmt->execute()) {    
-
-          echo "<li class='msg'><b>".ucwords($name).":</b> ".$text."</li>";
-        }
-
-        
+        $stmt->bindParam(':image', $new_name);
+        $stmt->execute();
       }
     }
-
+    if(isset($_POST['message']) && isset($_POST['toUser']) && isset($_POST['fromUser'])) {
+      $message = strip_tags(stripslashes($_POST['message']));
+      $fromUser = strip_tags(stripslashes($_POST['fromUser']));
+      $toUser = strip_tags(stripslashes($_POST['toUser']));
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $stmt = $pdo->prepare("INSERT INTO messages (message, to_user_id, from_user_id ) VALUES (:message, :toUser, :fromUser)");
+        $stmt->bindParam(':message', $message);
+        $stmt->bindParam(':toUser', $toUser);
+        $stmt->bindParam(':fromUser', $fromUser);
+        if($stmt->execute()) {
+          $query = $pdo->prepare("SELECT * FROM messages WHERE (to_user_id = '$toUser' or to_user_id = '$fromUser') && (from_user_id = '$fromUser' or from_user_id = '$toUser')");
+          $query ->execute();
+          while($fetch = $query->fetch(PDO::FETCH_ASSOC)){
+          $newmessage = $fetch['message'];
+          if ($fetch['from_user_id'] != $fromUser) {$class = 'in';} else {$class = 'out';};
+          echo "<p class='$class'>$newmessage</p>";
+         }
+        };  
+    }
+   
  ?>
