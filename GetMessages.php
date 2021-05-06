efd33694a0ec0fe7bea6870b01a45bffb4b8401e
Jarek <39769929+JarekWysocki@users.noreply.github.com>
2021-05-06 14:19:44 +0200

    Add files via upload

--- GetMessages.php
+++ GetMessages.php
@@ -4,7 +4,6 @@ require_once 'connect.php';
 
   $query = $pdo->prepare("SELECT * FROM messages ORDER BY id DESC");
   $query ->execute();
-
   while($fetch = $query->fetch(PDO::FETCH_ASSOC)){
     $nameid = $fetch['name'];
     $data = $pdo->prepare("SELECT * FROM tab1 WHERE id = '$nameid'");
@@ -19,16 +18,27 @@ require_once 'connect.php';
     $conn = $pdo->prepare("SELECT tab1.fname FROM likes INNER JOIN tab1 ON tab1.id=likes.who_like_id WHERE post_id = $idOfPost");
     $conn -> execute();
     $howmuchlikes = 0;
-    echo "<div class='wholikes'>";
     while($fetch2 = $conn->fetch(PDO::FETCH_ASSOC)){
     ++$howmuchlikes;
-    $wholikes = $fetch2['fname'];
-    echo "<p>$wholikes</p>";
     }
-    echo"</div>";
+   
     if($nameid) {
       if($img && $image) {
-            echo "<div id='$idOfPost' class='container'>
+        if ($howmuchlikes > 0) {
+            echo "<div class='container' id='$idOfPost'>
+            <img src='/img/$img'>
+            <p>$name</p>
+            <div>
+            <p>$message</p>
+            <img class='myphoto' src='/img/$image'>
+            <p class='like'>&#9733 like $howmuchlikes</p>
+            <p class='who'>check who</p>
+            <span class='time-right'>$time</span>
+            </div>
+            <form class='comments' onsubmit='return false;'><input class='comment' type='text'></form> 
+            </div>";}
+            else {
+              echo "<div class='container' id='$idOfPost'>
             <img src='/img/$img'>
             <p>$name</p>
             <div>
@@ -37,33 +47,79 @@ require_once 'connect.php';
             <p class='like'>&#9733 like $howmuchlikes</p>
             <span class='time-right'>$time</span>
             </div>
+            <form class='comments' onsubmit='return false;'><input class='comment' type='text'></form> 
             </div>";
+            }
       }
       if(!$img && $image) {
-        echo "<div id='$idOfPost' class='container'>
+        if ($howmuchlikes > 0) {
+        echo "<div class='container' id='$idOfPost'>
             <img src='/img/noimg.jpg'>
             <p>$name</p>
             <div>
             <p>$message</p>
             <img class='myphoto' src='/img/$image'>
             <p class='like'>&#9733 like $howmuchlikes</p>
+            <p class='who'>check who</p>
             <span class='time-right'>$time</span>
             </div>
-            </div>";
+            <form class='comments' onsubmit='return false;'><input class='comment' type='text'></form> 
+            </div>";}
+            else {
+              echo "<div class='container' id='$idOfPost'>
+              <img src='/img/noimg.jpg'>
+              <p>$name</p>
+              <div>
+              <p>$message</p>
+              <img class='myphoto' src='/img/$image'>
+              <p class='like'>&#9733 like $howmuchlikes</p>
+              <span class='time-right'>$time</span>
+              </div>
+              <form class='comments' onsubmit='return false;'><input class='comment' type='text'></form> 
+              </div>";
+            }
       }
       if ($img && !$image) {
-        echo "<div id='$idOfPost' class='container'>
+        if ($howmuchlikes > 0) {
+        echo "<div class='container' id='$idOfPost'>
             <img src='/img/$img'>
             <p>$name</p>
             <div>
             <p>$message</p>
             <p class='like'>&#9733 like $howmuchlikes</p>
+            <p class='who'>check who</p>
             <span class='time-right'>$time</span>
             </div>
-            </div>";
+            <form class='comments' onsubmit='return false;'><input class='comment' type='text'></form> 
+            </div>";}
+            else {
+              echo "<div class='container' id='$idOfPost'>
+              <img src='/img/$img'>
+              <p>$name</p>
+              <div>
+              <p>$message</p>
+              <p class='like'>&#9733 like $howmuchlikes</p>
+              <span class='time-right'>$time</span>
+              </div>
+              <form class='comments' onsubmit='return false;'><input class='comment' type='text'></form> 
+              </div>";
+            }
       }
       if (!$img && !$image) {
-        echo "<div id='$idOfPost' class='container'>
+        if ($howmuchlikes > 0) {
+        echo "<div class='container' id='$idOfPost'>
+            <img src='/img/noimg.jpg'>
+            <p>$name</p>
+            <div>
+            <p>$message</p>
+            <p class='like'>&#9733 like $howmuchlikes</p>
+            <p class='who'>check who</p>
+            <span class='time-right'>$time</span>
+            </div>
+            <form class='comments' onsubmit='return false;'><input class='comment' type='text'></form> 
+            </div>";}
+            else {
+              echo "<div class='container' id='$idOfPost'>
             <img src='/img/noimg.jpg'>
             <p>$name</p>
             <div>
@@ -71,7 +127,9 @@ require_once 'connect.php';
             <p class='like'>&#9733 like $howmuchlikes</p>
             <span class='time-right'>$time</span>
             </div>
+            <form class='comments' onsubmit='return false;'><input class='comment' type='text'></form> 
             </div>";
+            }
       }
     }
   }
